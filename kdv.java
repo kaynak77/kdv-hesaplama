af8543388f756afdcaeb3e29902cad3b19081d57
Yılmaz Kaynak <48969231+kaynak77@users.noreply.github.com>
2023-02-27 21:25:20 +0300

    Update kdv.java

--- kdv.java
+++ kdv.java
@@ -1,21 +1,26 @@
-     import java.util.Scanner;
-     public class kdv {
-     public static void main(String[] args) {
-      double tutar,kdvOranı=0.18,kdvtutar,kdvlitutar;
-       
-    
-      
-      Scanner input =new Scanner(System.in);
-      System.out.println("ücret tutarını giriniz:");
-      tutar =input.nextDouble();
+     import java'yı içe aktar kullanışlı _ tarayıcı ;
 
-      kdvtutar=tutar* kdvOranı;
-     kdvlitutar=tutar+ kdvtutar;
-     System.out.println("kdvsiz tutar:" +tutar);
-     System.out.println("kdvoranı:" +kdvOranı);
-     System.out.println("kdvtutarı:" +kdvtutar);
-     System.out.println("kdvlitutar:" +kdvlitutar);
-     
+genel  sınıf  ÜçgenAlanHesaplama {
+    genel  statik  geçersiz  ana ( String [] args ) {
+        Tarayıcı  taraması = yeni  Tarayıcı ( System . in );
+
+        sistem . dışarı _ yazdır ( "1. Kenar: " );
+        int  k1 = tarama . nextInt ();
+
+        sistem . dışarı _ print ( "2. Kenar: " );
+        int  k2 = tarama . nextInt ();
+
+        çift  ​​hipo = Matematik . sqrt ( k1 * k1 + k2 * k2 );
+        sistem . dışarı _ println ( "Hipotenüs: " + hipo );
+        
+        çift  ​​u = ( k1 + k2 + hipo ) / 2 ;
+        çift  ​​alan = Matematik . sqrt ( u * ( u - k1 ) * ( u - k2 ) * ( u - hipo ));
+
+        sistem . dışarı _ printf ( "Alan:%.1f" , alan );
+
+        taramak _ kapat ();
+    }
+}
 
 
 
